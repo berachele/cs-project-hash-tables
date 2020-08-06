b3536df15d7e5358cc0523ea0dcadf9e5271c27a
Rachele Edwards <dot.com1472@gmail.com>
2020-08-06 16:10:32 -0600

    Im getting better (explanation point) Yaay, able to finish dup words and pass test

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,19 @@
 def no_dups(s):
     # Your code here
+    #split string into words
+    words = s.split()
+    #assign list for duplicate words
+    dups = []
+    #final string
+    string = ""
+    #add word to dups list if not already in there
+    for word in words:
+        if word not in dups:
+            dups.append(word)
+    #combine dups list into string again
+    string += " ".join(dups)
 
+    return string
 
 
 if __name__ == "__main__":
