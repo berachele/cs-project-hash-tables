4eacabb0607d4862174efd9150066e21ee2a1966
Rachele Edwards <dot.com1472@gmail.com>
2020-08-06 15:14:00 -0600

    I have it counting, some are right, but not with capital lettered words, even with .lower(

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,26 @@
 def word_count(s):
     # Your code here
+    symbols = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    #ignore spaces and symbols
+    for i in symbols:
+        s = s.replace(i, "")
+    #all lowercase
+    s.lower()
+    #split each word based on space, ignoring spaces as well
+    words = s.split()
+    counter = {}
+    #if empty, return {}
+    for word in words:
+        word.lower()
+        #add to counter if word is there
+        if word not in counter:
+            print(f'{word}: only one')
+            counter[word] = 0
+            #otherwise counter is 1 for one appearance
+        print(f'{word}: Adding one')
+        counter[word] += 1
+
+    return counter
 
 
 
