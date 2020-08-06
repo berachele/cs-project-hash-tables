332e24fbb6e05559502138a4b145437534770234
Rachele Edwards <dot.com1472@gmail.com>
2020-08-06 15:33:49 -0600

    word count is passing

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,25 +1,23 @@
 def word_count(s):
     # Your code here
-    symbols = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+    symbols = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&', '\r', '\n', '\t', '\\' ]
     #ignore spaces and symbols
     for i in symbols:
-        s = s.replace(i, "")
+        s = s.replace(i, " ")
     #all lowercase
     s.lower()
     #split each word based on space, ignoring spaces as well
-    words = s.split()
+    words = s.lower().split()
     counter = {}
-    #if empty, return {}
     for word in words:
-        word.lower()
+        # word.lower()
         #add to counter if word is there
         if word not in counter:
-            print(f'{word}: only one')
+            # print(f'{word}: only one')
             counter[word] = 0
             #otherwise counter is 1 for one appearance
-        print(f'{word}: Adding one')
+        # print(f'{word}: Adding one')
         counter[word] += 1
-
     return counter
 
 
@@ -29,3 +27,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
